868ac624e1a9a930b4c263f8eccc6e12324dcc76
Egleman <martsnov@hotmail.com>
2022-01-11 14:44:15 +0300

    Выполнение дополнительного задания

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -11,7 +11,16 @@ const comments = () => {
     const commentsCom2 = document.querySelector('.comments-comm-2');
     const commentsCom3 = document.querySelector('.comments-comm-3');
 
-    let newComments = [];
+    let newComments = {};
+    let arrUser = [];
+
+    class NoComments {
+        constructor (elem) {
+            this.author = elem.author;
+            this.image = elem.image;
+            this.comment = elem.comment;
+        }
+    }
 
     const getData = (data) => {
         fetch('./comments.json')
@@ -23,45 +32,44 @@ const comments = () => {
             console.log(err, 'ошибка, файл не найден');
         });
     };
-
-    const render = () => {
-        imgAvatar1.src = `images/users/${newComments[0].image}`;
-        imgAvatar2.src = `images/users/${newComments[1].image}`;
-        imgAvatar3.src = `images/users/${newComments[2].image}`;
-
-        commentsName1.textContent = newComments[0].author;
-        commentsName2.textContent = newComments[1].author;
-        commentsName3.textContent = newComments[2].author;
-
-        commentsCom1.textContent = newComments[0].comment;
-        commentsCom2.textContent = newComments[1].comment;
-        commentsCom3.textContent = newComments[2].comment;
-
-        setInterval(() => {
-            newComments.push(newComments.unshift());
-            imgAvatar1.src = `images/users/${newComments[0].image}`;
-            imgAvatar2.src = `images/users/${newComments[1].image}`;
-            imgAvatar3.src = `images/users/${newComments[2].image}`;
-
-            commentsName1.textContent = newComments[0].author;
-            commentsName2.textContent = newComments[1].author;
-            commentsName3.textContent = newComments[2].author;
-
-            commentsCom1.textContent = newComments[0].comment;
-            commentsCom2.textContent = newComments[1].comment;
-            commentsCom3.textContent = newComments[2].comment;
-            //console.log(newComments);
-        }, 1000);
+    const swapItems = (nextArray) => {
+        const temp = nextArray[0];
+        nextArray.push(temp);
+        nextArray.splice(0,1);
+        return nextArray;
     };
 
-    getData((data) => {
-        data.comments.forEach(item => {
-            newComments.push(item);
+    const render = () => {
+        let arrayCopy = Object.assign([], arrUser);
+        let swappedArray = swapItems(arrayCopy);
+        arrUser.splice(0);
+        swappedArray.forEach(item => {
+            arrUser.push(item);
         });
-        //render();
+        imgAvatar1.src = `./images/users/${arrUser[0].image}`;
+        imgAvatar2.src = `./images/users/${arrUser[1].image}`;
+        imgAvatar3.src = `./images/users/${arrUser[2].image}`;
+
+        commentsCom1.textContent = arrUser[0].comment;
+        commentsCom2.textContent = arrUser[1].comment;
+        commentsCom3.textContent = arrUser[2].comment;
+
+        commentsName1.textContent = arrUser[0].author;
+        commentsName2.textContent = arrUser[1].author;
+        commentsName3.textContent = arrUser[2].author;
+    };
+
+    getData(data => {
+        newComments = data.comments.map(item => new NoComments(item));
+        newComments.forEach(item => {
+            arrUser.push(item);
+        });
+        render();
     });
-    //newComments.splice(0,1);
-    console.log(newComments);
+    setInterval(()=> {
+        render();
+    }, 20000);
+    
 };
 
 export default comments;
